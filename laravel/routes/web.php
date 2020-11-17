231ffe4aa83402e38535b1db09b8b07242f74303
syumai013 <syumai3875@gmail.com>
2020-11-17 21:36:35 +0900

    first laravel commit

---
+++ laravel/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+Route::get('/','ArticleController@index')->name('articles.index');
+Route::resource('/articles','ArticleController')->except(['index', 'show'])->middleware('auth');
+Route::resource('/articles','ArticleController')->only(['show']);
+Route::prefix('articles')->name('articles.')->group(function () {
+  Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
+  Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
+});
+Route::get('/tags/{name}', 'TagController@show')->name('tags.show');
+Route::prefix('users')->name('users.')->group(function() {
+  Route::get('/{name}', 'UserController@show')->name('show');
+  Route::get('/{name}/likes', 'UserController@likes')->name('likes');
+  Route::get('/{name}/followings', 'UserController@followings')->name('followings');
+  Route::get('/{name}/followers', 'UserController@followers')->name('followers');
+  Route::middleware('auth')->group(function () {
+    Route::put('/{name}/follow', 'UserController@follow')->name('follow');
+    Route::delete('/{name}/follow', 'UserController@unfollow')->name('unfollow');
+});
+});
